598d415f51363eef23e5e8d3db7571bc29886194
Claudio Sacerdoti Coen <claudio.sacerdoticoen@unibo.it>
2024-07-13 20:54:44 +0200

    First experiments with Matita declarative syntax for Lean

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import «VerboseMatita»
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ lakefile.lean
@@ -0,0 +1,14 @@
+import Lake
+open Lake DSL
+
+package «verbose-matita» where
+  -- add package configuration options here
+
+lean_lib «VerboseMatita» where
+  -- add library configuration options here
+
+@[default_target]
+lean_exe «verbose-matita» where
+  root := `Main
+
+require verbose from git "https://github.com/PatrickMassot/verbose-lean4.git"

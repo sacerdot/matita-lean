38969cde723f5909a31e294849ae932c3c78e681
Claudio Sacerdoti Coen <claudio.sacerdoticoen@unibo.it>
2024-07-15 17:23:12 +0200

    do not compile an exe

--- lakefile.lean
+++ lakefile.lean
@@ -4,11 +4,12 @@ open Lake DSL
 package «verbose-matita» where
   -- add package configuration options here
 
+@[default_target]
 lean_lib «VerboseMatita» where
   -- add library configuration options here
 
-@[default_target]
-lean_exe «verbose-matita» where
-  root := `Main
+--@[default_target]
+--lean_exe «verbose-matita» where
+--  root := `Main
 
 require verbose from git "https://github.com/PatrickMassot/verbose-lean4.git"
